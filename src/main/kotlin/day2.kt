5cc961eb0c9deb21bf97330f6531b4283f9bb1f2
Simon Klausner <simon.klausner@nttdata.com>
2022-12-02 08:51:35 +0100

    initial commit

--- src/main/kotlin/day2.kt
+++ src/main/kotlin/day2.kt
@@ -2,27 +2,64 @@ import java.io.File
 
 fun main() {
 
-    fun parseCalories(): List<Int> {
-        val calories = mutableListOf(0)
-        File("resources/day1.txt").forEachLine { line ->
-            if (line.isBlank()) {
-                calories.add(0)
-                return@forEachLine
-            }
-
-            calories[calories.lastIndex] = calories[calories.lastIndex] + line.toInt()
+    fun String.rpsToInt(): Int {
+        return when {
+            this == "A" || this == "X" -> 0
+            this == "B" || this == "Y" -> 1
+            else -> 2
         }
-        return calories
+    }
+
+    // returns RPS for right elf to get expected outcome
+    fun String.toRps(otherElf: Int): Int {
+        return when {
+            this == "X" -> (otherElf + 2) % 3
+            this == "Y" -> otherElf
+            else -> (otherElf + 1) % 3
+        }
+    }
+
+    // calculates score for right elf
+    fun rpsResult(left: Int, right: Int): Int {
+        return when {
+            left == right -> 3
+            (left + 1) % 3 == right -> 6
+            else -> 0
+        }
     }
 
     fun part1() {
-        val calories = parseCalories()
-        println(calories.max())
+        fun parseRounds(): List<Pair<Int, Int>> {
+            val rounds = mutableListOf<Pair<Int, Int>>()
+            File("resources/day2.txt").forEachLine { line ->
+                val roundInput = line.split(" ")
+                val leftElf = roundInput[0].rpsToInt()
+                val rightElf = roundInput[1].rpsToInt()
+
+                rounds += Pair(leftElf, rightElf)
+            }
+            return rounds
+        }
+
+        val rounds = parseRounds()
+        println(rounds.sumOf { (left, right) -> rpsResult(left, right) + right + 1 })
     }
 
     fun part2() {
-        val calories = parseCalories()
-        println(calories.sortedDescending().take(3).sum())
+        fun parseRounds(): List<Pair<Int, Int>> {
+            val rounds = mutableListOf<Pair<Int, Int>>()
+            File("resources/day2.txt").forEachLine { line ->
+                val roundInput = line.split(" ")
+                val leftElf = roundInput[0].rpsToInt()
+                val rightElf = roundInput[1].toRps(leftElf)
+
+                rounds += Pair(leftElf, rightElf)
+            }
+            return rounds
+        }
+
+        val rounds = parseRounds()
+        println(rounds.sumOf { (left, right) -> rpsResult(left, right) + right + 1 })
     }
 
     part1()
